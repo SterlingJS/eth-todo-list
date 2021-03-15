9ccc8c71c9672db8c672a99733c5530336ea4385
Jim <jws6720@rit.edu>
2021-03-15 19:29:10 -0400

    Initial commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.5.0;
+
+contract TodoList {
+	uint public taskCount = 0; // State variables are written to the blockchain. These are like class variables since they belong to the entire contract
+
+}
